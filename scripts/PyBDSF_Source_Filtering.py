deebcd3ddb499a2aa6bb2d5564f715648d4d5a57
AKHughes1994 <49698839+AKHughes1994@users.noreply.github.com>
2023-08-27 13:54:57 -0600

    Update PyBDSF_Source_Filtering.py

--- scripts/PyBDSF_Source_Filtering.py
+++ scripts/PyBDSF_Source_Filtering.py
@@ -12,6 +12,9 @@ cfg = configparser.ConfigParser()
 cfg.read('config.ini')
 
 sourceCoords = SkyCoord(cfg['POSITIONS']['source_ra'],cfg['POSITIONS']['source_dec'], frame='icrs')
+flux_threshold   = cfg['THRESHOLDS']['flux_threshold']
+size_threshold   = cfg['THRESHOLDS']['size_threshold']
+target_threshold = cfg['THRESHOLDS']['target_threshold']
 
 # Load date-time array
 date_times = np.load('../files/date-times.npy')
