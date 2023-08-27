7c8de6b4a848a8785d3dfc2fc05049e53a6cb2c1
AKHughes1994 <49698839+AKHughes1994@users.noreply.github.com>
2023-08-27 13:44:05 -0600

    Update PyBDSF_Source_Extraction.py

--- scripts/PyBDSF_Source_Extraction.py
+++ scripts/PyBDSF_Source_Extraction.py
@@ -3,7 +3,7 @@ from astropy.io import fits
 import bdsf #PyBDSF
 import numpy as np
 
-#Read in the configuration file
+# Read in the configuration file
 cfg = configparser.ConfigParser()
 cfg.read('config.ini')
 
@@ -20,29 +20,27 @@ def str_to_bool(s):
 
 fix_to_beam = str_to_bool(cfg['PYBDSF']['fix_to_beam'])
 
-#Extract the source name, observing times, and order according to date
+# Extract the source name, observing times, and order according to date
 image_names = glob.glob('../images/*.fits') #get image names
 for image_name in image_names:
     hdul = fits.open(image_name) #load image
     date_times.append(hdul[0].header['DATE-OBS'])
     hdul.close()
 
-#Ensure it's ordered in time
+# Ensure it's ordered in time
 sorted_index = sorted(range(len(date_times)), key = date_times.__getitem__)
 date_times = np.array(date_times)[sorted_index]
 image_names = np.array(image_names)[sorted_index]
 
-#Save the date-times for future scripts
+# Save the date-times for future scripts
 np.save('../files/date-times', date_times)
 np.save('../files/image-names',image_names)
 
-'''
-#Run each image through PyBDSF
+# Run each image through PyBDSF
 for image_name,date_time in zip(image_names,date_times): #All images
 
-    #PyBDSF run
+    # PyBDSF run
     img = bdsf.process_image(image_name,thresh_pix=float(cfg['PYBDSF']['snr_threshold']),fix_to_beam=fix_to_beam, minpix_isl=5.0,  rms_box=(300,100)) #trim_box=(7750,8250,7750,8250))
 
-    #Save FITS catalog
+    # Save FITS catalog
     img.write_catalog(format='fits', catalog_type='srl', outfile = '../files/total_field_%s_%s.fits' %(date_time,cfg['SOURCE']['name']), clobber=True)
-'''
